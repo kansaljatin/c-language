158d5bf2b42abcadd867850c6e93190f55bafe68
animesh10-12 <54526121+animesh10-12@users.noreply.github.com>
2019-10-11 19:08:25 +0530

    conservation of sec

---
+++ conversion of sec.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int s,m,h,s1;
+	printf("enter the no of seconds\n");
+	scanf("%d",&s);
+	h=s/3600;
+	m=(s%3600)/60;
+	s1=(s%3600)%60;
+	printf("hour:%d min:%d sec:%d",h,m,s1);
+	return 0;
+}
