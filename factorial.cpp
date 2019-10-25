1e990a8629358b508e74537b81f2a979ca4bfe63
jatin kansal <56547404+kansaljatin@users.noreply.github.com>
2019-10-25 11:37:35 -0700

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -4,10 +4,14 @@ int main()
 {
 	int i,n,fac;
 	scanf("%d",&n);
-	fac=1;
+	if(n>0)
+	{fac=1;
 	for(i=1;i<=n;i++)
 	{
 		fac=fac*i;
+	}}
+	else
+	{ printf("invalid no.");
 	}
 	printf("%d\n",fac);
 	return 0;
