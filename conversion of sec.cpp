bd2e0766545ced60c6353cf125dbf381e4a48758
jatin kansal <56547404+kansaljatin@users.noreply.github.com>
2019-10-25 11:42:05 -0700

    Update conversion of sec.cpp

--- conversion of sec.cpp
+++ conversion of sec.cpp
@@ -5,9 +5,13 @@ int main()
 	int s,m,h,s1;
 	printf("enter the no of seconds\n");
 	scanf("%d",&s);
-	h=s/3600;
-	m=(s%3600)/60;
-	s1=(s%3600)%60;
-	printf("hour:%d min:%d sec:%d",h,m,s1);
+	if(s>=0)
+	{	h=s/3600;
+		m=(s%3600)/60;
+		s1=(s%3600)%60;}
+	else
+	{	printf("invalid no. of seconds as time can never be zero");
+	}
+	 printf("hour:%d min:%d sec:%d",h,m,s1);
 	return 0;
 }
